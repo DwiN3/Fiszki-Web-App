5bb5e18b5591142c4c38791c147dc2cc19676f2d
DwiN3 <dwinicht333@gmail.com>
2023-11-08 10:02:52 +0100

    dodanie projektu

---
+++ fiszki-server/src/main/java/com/postgresql/fiszkiserver/FiszkiServerApplication.java
@@ -0,0 +1,13 @@
+package com.postgresql.fiszkiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiszkiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiszkiServerApplication.class, args);
+	}
+
+}
---
+++ fiszki-server/src/test/java/com/postgresql/fiszkiserver/FiszkiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postgresql.fiszkiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiszkiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
