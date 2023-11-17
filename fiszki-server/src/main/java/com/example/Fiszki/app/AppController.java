6048df6bcb6d723cf0696c3b5246c0f086d1fcd4
DwiN3 <dwinicht333@gmail.com>
2023-11-17 12:10:54 +0100

    dodanie fiszki

--- fiszki-server/src/main/java/com/example/Fiszki/app/AppController.java
+++ fiszki-server/src/main/java/com/example/Fiszki/app/AppController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/app-controller")
-public class appController {
+public class AppController {
     TokenInstance tokenInstance = TokenInstance.getInstance();
 
     @GetMapping
---
+++ fiszki-server/src/main/java/com/example/Fiszki/models/Flashcards.java
@@ -0,0 +1,47 @@
+package com.example.Fiszki.models;
+
+public class Flashcards {
+    private String collectionName, language, category, word, translatedWord, example, translatedExample;
+
+    public Flashcards() {
+    }
+
+    public Flashcards(String collectionName, String language, String category, String word, String translatedWord, String example, String translatedExample) {
+        this.collectionName = collectionName;
+        this.language = language;
+        this.category = category;
+        this.word = word;
+        this.translatedWord = translatedWord;
+        this.example = example;
+        this.translatedExample = translatedExample;
+    }
+
+
+    public String getCollectionName() {
+        return collectionName;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public String getTranslatedWord() {
+        return translatedWord;
+    }
+
+    public String getExample() {
+        return example;
+    }
+
+    public String getTranslatedExample() {
+        return translatedExample;
+    }
+}
