479bc2dc8a3d181e3cd08cb75a549111edaa2fac
MichalPolak01 <michalpl618@gmail.com>
2024-01-04 23:29:53 +0100

    Dodanie cors do security

--- fiszki-server/src/main/java/com/example/Fiszki/security/config/SecurityConfig.java
+++ fiszki-server/src/main/java/com/example/Fiszki/security/config/SecurityConfig.java
@@ -12,8 +12,62 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 
+//@Configuration
+//@EnableWebSecurity
+//@RequiredArgsConstructor
+//public class SecurityConfig {
+//
+//    private final JwtAuthenticationFilter jwtAuthFilter;
+//    private final AuthenticationProvider authenticationProvider;
+//
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer()
+//    {
+//            return new WebMvcConfigurer()
+//            {
+//                public void addCorsMappings(final CorsRegistry registry)
+//                {
+//                    registry
+//                            .addMapping("/**")
+//                            .allowedOrigins("http://localhost:4200")
+//                            .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS")
+//                            .allowedHeaders("*")
+//                            .allowCredentials(true)
+//                            .maxAge(3600);
+//                }
+//            };
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+//
+//        //                        .loginPage("/login")
+//        httpSecurity
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .authorizeHttpRequests((requests) -> requests
+//                        .requestMatchers(new AntPathRequestMatcher("/flashcards/auth/**")).permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .sessionManagement((sessionManagement) -> sessionManagement
+//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                )
+//                .authenticationProvider(authenticationProvider)
+//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+////                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll);
+//
+//
+//        return httpSecurity.build();
+//    }
+//}
 
 @Configuration
 @EnableWebSecurity
@@ -24,10 +78,38 @@ public class SecurityConfig {
     private final AuthenticationProvider authenticationProvider;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            public void addCorsMappings(final CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
 
-        //                        .loginPage("/login")
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        //Make the below setting as * to allow connection from any hos
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+                .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource())) // Dodaj tę linię dla obsługi CORS
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers(new AntPathRequestMatcher("/flashcards/auth/**")).permitAll()
@@ -38,9 +120,8 @@ public class SecurityConfig {
                 )
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-//                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll);
-
 
         return httpSecurity.build();
     }
 }
+
