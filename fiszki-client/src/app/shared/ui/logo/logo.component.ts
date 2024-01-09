ec4e8c0e9dfb08d5544d2c60057d2cabe82f26b7
q1bczyk <bartekkubik7@gmail.com>
2024-01-09 20:45:52 +0100

    podlinkowanie logo

--- fiszki-client/src/app/shared/ui/logo/logo.component.ts
+++ fiszki-client/src/app/shared/ui/logo/logo.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logo',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./logo.component.scss']
 })
 export class LogoComponent {
+
+  constructor(private router : Router){}
+
+  Navigate() : void
+  {
+    this.router.navigate(['user']);
+  }
 
 }
